da71e747c3484b5ad639388b8be9db904661e9ac
carecodeconnect <carecodeconnect@pm.me>
2024-04-01 14:51:27 +0200

    update transcriber

--- src/transcriber.py
+++ src/transcriber.py
@@ -1,19 +1,3 @@
-# import wave
-# import whisper
-
-# def transcribe_audio(filename, model_name="medium.en"): # tiny.en or small.en or medium.en
-#     """
-    
-#     """
-    
-#     # Load the Whisper model and transcribe the audio file
-#     model = whisper.load_model(model_name)
-#     result = model.transcribe(filename)
-    
-#     # Print the transcribed prompt
-#     print(f"Instruction given by the user: {result['text']}")
-    
-#     return result['text']
 import wave
 
 def transcribe_audio(filename, model):
